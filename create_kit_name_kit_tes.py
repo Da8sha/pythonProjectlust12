d53270bd6ba3eb48b0b010850c7c69a68081a7ce
Da8sha <125490110+Da8sha@users.noreply.github.com>
2023-02-26 19:36:45 +0300

    Update create_kit_name_kit_tes.py

--- create_kit_name_kit_tes.py
+++ create_kit_name_kit_tes.py
@@ -1,4 +1,15 @@
+import data
 import sender_stand_request
+def test_proverka_sozdanie_zakaza_200():
+    create_order_response = sender_stand_request.post_new_order(data)
+    assert create_order_response.status_code == 201 , "Ошибка создания заказа"
+
+def test_nalichie_nomera_treka():
+    assert (sender_stand_request.response.json())
+
+def test_poluchienie_nomera_treka():
+    print (sender_stand_request.response.json())
+
 def test_proverka_200():
     true_proverka =sender_stand_request.response_2
     assert true_proverka.status_code==200
